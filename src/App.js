e0feed4f8ab6bc0083310fe3ae51b22da08efe42
ferdeveloper <fernandoarenasdev@gmail.com>
2017-05-20 09:47:12 +0200

    First commit.
    React, stylus and eslint are added to project.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Index">
+        <div className="Index-contact">
+          <h1>Fernando Arenas</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
