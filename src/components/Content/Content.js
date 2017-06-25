c714d9a0dc34e9ad6395f16bb5a56a08f6d71132
ferdeveloper <fernandoarenasdev@gmail.com>
2017-06-25 21:10:07 +0200

    TabSelector component changed for react-tabs package

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import PercentLines from '../PercentLines/PercentLines';
-import TabSelector from '../TabSelector/TabSelector';
 import percentageData from '../../resources/data/mySkills.json';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class Content extends React.Component {
 
+  getPercentLines() {
+    return (
+      <PercentLines
+        percentageData={percentageData}
+        textWidth={'100px'}
+      />
+    );
+  }
+
   getTabs = () => {
+
     const tabs = [
       {
-        tabName: 'Sobre mí',
+        name: 'Sobre mí',
         component: <p>Holi!!!</p>,
       },
       {
-        tabName: 'Skills',
-        component: <p>Skills!!!</p>,
+        name: 'Skills',
+        component: this.getPercentLines(),
       },
     ];
 
-    return tabs;
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>{'Sobre mí'}</Tab>
+          <Tab>{'Skills'}</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          {this.getPercentLines()}
+        </TabPanel>
+      </Tabs>
+    );
   }
 
   render() {
     return (
       <div className="Content" >
-        <TabSelector
-          tabs={this.getTabs()}
-        />
+        {this.getTabs()}
         {/*
           <PercentLines
             percentageData={percentageData}
